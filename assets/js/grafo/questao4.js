ac09551e6841c828c6e9ea2f78838bda4ec85cb2
Alessandro <ale-remo@hotmail.com>
2016-08-29 09:28:03 -0300

    Tabela de performance da 4º questão adicionada

--- assets/js/grafo/questao4.js
+++ assets/js/grafo/questao4.js
@@ -38,18 +38,18 @@ var atualizarGrafico = function(dados) {
                 label: "Performance Matriz",
                 fill: false,
                 lineTension: 0.1,
-                backgroundColor: "rgba(75,192,192,0.4)",
-                borderColor: "rgba(75,192,192,1)",
+                backgroundColor: "#757575",
+                borderColor: "#757575",
                 borderCapStyle: 'butt',
                 borderDash: [],
                 borderDashOffset: 0.0,
                 borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(75,192,192,1)",
+                pointBorderColor: "#C51162",
                 pointBackgroundColor: "#fff",
                 pointBorderWidth: 1,
                 pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBackgroundColor: "#C51162",
+                pointHoverBorderColor: "#C51162",
                 pointHoverBorderWidth: 2,
                 pointRadius: 1,
                 pointHitRadius: 10,
@@ -60,18 +60,18 @@ var atualizarGrafico = function(dados) {
                 label: "Performance Lista",
                 fill: false,
                 lineTension: 0.1,
-                backgroundColor: "rgba(192,75,75,0.4)",
-                borderColor: "rgba(192,75,75,1)",
+                backgroundColor: "#B2DFDB",
+                borderColor: "#B2DFDB",
                 borderCapStyle: 'butt',
                 borderDash: [],
                 borderDashOffset: 0.0,
                 borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(192,75,75,1)",
+                pointBorderColor: "#B2DFDB",
                 pointBackgroundColor: "#fff",
                 pointBorderWidth: 1,
                 pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(192,75,75,1)",
-                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBackgroundColor: "#B2DFDB",
+                pointHoverBorderColor: "#B2DFDB",
                 pointHoverBorderWidth: 2,
                 pointRadius: 1,
                 pointHitRadius: 10,
@@ -101,10 +101,56 @@ var atualizarGrafico = function(dados) {
               }
         }
     });
-}
+};
+
+var atualizarTabelaPerformance = function(dados) {
+    var tabelaBody = $('#tabelaPerformance tbody');
+
+    tabelaBody.empty();
+
+    for (var i in dados.labels) {
+        tabelaBody.append(getRowTabelaPerformance(i, dados.performanceMatriz[i], dados.performanceLista[i]));
+    }
+};
+
+var getRowTabelaPerformance = function(vertice, performanceMatriz, performanceLista) {
+    var tr = document.createElement('tr');
+
+    tr.appendChild(getTableCell(vertice));
+
+    var cellPerformanceMatriz = getTableCell(performanceMatriz);
+    var cellPerformanceLista = getTableCell(performanceLista);
+
+    if (performanceMatriz > performanceLista) {
+        cellPerformanceMatriz.className = 'danger';
+        cellPerformanceLista.className = 'success';
+    } else if (performanceMatriz < performanceLista){
+        cellPerformanceMatriz.className = 'success';
+        cellPerformanceLista.className = 'danger';
+    } else {
+        cellPerformanceMatriz.className = 'info';
+        cellPerformanceLista.className = 'info';
+    }
+
+    tr.appendChild(cellPerformanceMatriz);
+    tr.appendChild(cellPerformanceLista);
+
+    return tr;
+};
+
+var getTableCell = function (content) {
+    var cell = document.createElement('td');
+
+    cell.appendChild(document.createTextNode(content));
+
+    return cell;
+
+};
 
 $(function() {
     var dadosGrafico = getGraphicData();
 
     atualizarGrafico(dadosGrafico);
+
+    atualizarTabelaPerformance(dadosGrafico);
 });
